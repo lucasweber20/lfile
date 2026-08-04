1f1bef266a38181e5e000a5431f9a0628737f230
lucasweber20 <lucasweber449@gmail.com>
2026-08-04 07:55:03 -0300

    Feat: add arguments flags

--- lfile.py
+++ lfile.py
@@ -1,7 +1,21 @@
+import argparse
 
+
+parser = argparse.ArgumentParser()
+
+args = parser.add_argument("-u", "--url", help='Specify url, example: -u https://example.com/?param=value', type=str)
+args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
+args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 2", type=int)
+args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
+
+args = parser.parse_args()
 
 def main():
-    pass
+    # Flags
+    url = args.url
+    file = args.list
+    thread = args.thread
+    output = args.output
 
 if __name__ == "__init__":
     main()
