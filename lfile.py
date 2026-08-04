111a25b80811e9226a2121e73fb9656097901965
lucasweber20 <lucasweber449@gmail.com>
2026-08-04 09:30:52 -0300

    Feat: requests urls

--- lfile.py
+++ lfile.py
@@ -1,13 +1,15 @@
 import argparse
+import concurrent.futures
 from scripts.URL import URL
 from scripts.Parser import Parser
+from scripts.Requests import Requests
 
 
 parser = argparse.ArgumentParser()
 
 args = parser.add_argument("-u", "--url", help='Specify url, example: -u https://example.com/?param=value', nargs="+", type=str)
 args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
-args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 2", type=int)
+args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 2", default=1, type=int)
 args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
 
 args = parser.parse_args()
@@ -26,11 +28,22 @@ def main():
         url = urls.remove_duplicates()
 
     # Parser
+    parsed_urls_params = []
     for parser_url in url:
         parser = Parser(parser_url)
         payload_read = open("./db/payloads.txt").read().splitlines()
         for payload in payload_read:
             parsed_urls = parser.parser_params(payload)
+            parsed_urls_params.append(parsed_urls)
+
+    # Requests
+    req = Requests()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=thread) as executor:
+        futures = [executor.submit(req.requests, url) for url in parsed_urls_params]
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if "root:" in result:
+                print("Directory traversal found!")
 
 if __name__ == "__main__":
     main()
---
+++ scripts/Requests.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class Requests:
+    def __init__(self):
+        pass
+
+    def requests(self, url):
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"}
+        try:
+            req = requests.get(url, headers=headers, allow_redirects=False, timeout=6)
+            text = req.text
+            return text
+        except:
+            pass
